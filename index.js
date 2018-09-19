974a2bcf5f961f19f4ae61d341b2df9882672c64
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-20 01:35:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.login(process.env.TOKEN)
 
 client.on('message', message => {
 if (message.content.startsWith("-sondage")){
-  if(message.author.id == ""){
+ // if(message.author.id == ""){
     let args = message.content.split(" ").slice(1);
     let thingToEcho = args.join("")
     var embed = new Discord.RichEmbed()
@@ -28,7 +28,7 @@ if (message.content.startsWith("-sondage")){
     });
   }else{
     return message.reply("Tu n'as pas la permission.")
-}}})
+}})
 
 client.on('message', message => {
     if (message.content === '-help') {   
