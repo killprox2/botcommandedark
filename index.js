383eee9c01d76f9c813c51b64aacb61f29a4ede3
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-19 21:55:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,12 +42,13 @@ client.on('message', message => {
         let reason = args.slice(0).join(" ");
         let hereRole = message.guild.roles.find("name", "Modérateur Discord");
               
-        if(!reason) reason = "Aucune raison";
-        message.channel.send('Aucune raison écrite')
+        
         if(reason)
         message.delete()
         message.guild.channels.find("name", "bugs").send(hereRole +` Salut ` + message.member.displayName + ` rapport un bug le voici: ${reason}.`);
-  
+        if(!reason) reason = "Aucune raison";
+        message.delete()
+        message.channel.send('Aucune raison écrite')
   
          //   message.delete()
            // message.guild.channels.find("name", "bugs").send(hereRole +` Salut ` + message.member.displayName + ` rapport un bug le voici: ${reason}.`);
