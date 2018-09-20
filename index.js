77435e949df414caa8fe495ba08875aeac4abda2
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-20 16:16:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,25 +22,6 @@ client.on('message', message => {
   message.channel.sendEmbed(embed)
   };
 })
-client.on('message', message => {
-  if (message.content === '-google') {   
-    let google = args.slice(0).join('+');
-
-    let link = `https://www.google.com/search?q=` + google;
-    if(!link)return message.reply("Console error")
-    let embed = new Discord.RichEmbed()
-
-.setColor("RED")
-.setTimestamp()
-.addField('Action:', 'Searching on Google')
-.addField("Word:", `${args.slice(0).join(' ')}`)
-.addField('Link:', `${link}`)
-.setFooter("You're avatar", message.author.avatarURL);
-      
-message.channel.send(embed);
-message.author.send(`You have searched for ${link} in ${ message.guild.name}`);
-  };
-})
 
 client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
