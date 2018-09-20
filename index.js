fefa6cef1b0eae58e7e60a854b351d79cec32b86
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-20 16:12:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,20 @@ client.on('message', message => {
 })
 client.on('message', message => {
   if (message.content === '-cf') {   
-    message.channel.send(`Resultat: **${Math.floor(Math.random() * 2) == 0 ? "CT" : "T"}**!`);
+    let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!hugUser) return message.channel.send("Make sure you mention someone!");
+
+    const {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/hug`);
+
+    let hugEmbed = new Discord.RichEmbed()
+    .setTitle("Hug! c:")
+    .setDescription(`**${message.author.username}** hugged **${message.mentions.users.first().username}**!`)
+    .setImage(body.url)
+    .setColor("RANDOM")
+    .setFooter("Bot Version: 0.0.4", bot.user.displayAvatarURL);
+
+    message.channel.send(hugEmbed)
   };
 })
 
