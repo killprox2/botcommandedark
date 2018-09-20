0e5a22e0a8a4e86894b78286604ad823d39c8a5a
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-21 00:17:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('message', message => {
       .setDescription("Permet de connaitre toutes les commandes")
       .setColor("0xB40404") 
       .addField("**-fm** :arrow_forward: ***object_souhaite Informations_sur_ce_que_vous_voulez_faire_comme_fm***", "Permet de passer une commande a un forgemage")
-      .addField("**-bug** :arrow_forward: ***decrire_le_bug***", "Permet de rapporter un bug sur le bot")
+      .addField("**-bug** :heavy_minus_sign: ***decrire_le_bug***", "Permet de rapporter un bug sur le bot")
       .addField("**-idee** :arrow_forward: ***votre_idee***", "Permet de donner vos idée pour le bot")
       .addField("**-info**", "Permet de connaitre les infos du Discord")
       .setImage("https://i.imgur.com/A1wcXrl.png")
