d8d8edd04b23f1306002df960464829e4996e397
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-20 12:42:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,20 +30,21 @@ if (message.content === '-sondage'){
     return message.reply("Tu n'as pas la permission.")
 }}})*/
 client.on('message', message => {            
-  if (message.content === '-sondage'){ 
+  if (message.content.startsWith(prefix + 'sondage')){ 
     let args = message.content.split(" ").slice(1);
-   // let thingToEcho = args.join(" ")
+    let thingToEcho = args.join(" ")
     var embed = new Discord.RichEmbed()
       .setTitle("Sondage")
-      .addField("Répondre avec :white_check_mark: ou :x: ")
+      .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x: ")
       .setColor("0xB40404")
       .setTimestamp()
   message.channel.sendEmbed(embed)
   .then(function (message){
-    message.react(":white_check_mark:")
-    message.react(":x:")
-   })
-  }})
+    message.react("Oui")
+    message.react("Non")
+   }).catch(function(){
+
+   });
 
   client.on('message', message => {
     if (message.content === '-help2') {   
