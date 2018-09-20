4ff92e3246b398432819170553dcf7c4b4492a08
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-20 02:20:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ client.on('ready', function () {
 
 client.login(process.env.TOKEN)
 
+/*client.on('message', message => {
+if (message.content.startsWith("-sondage")){
+  if(message.guild.id == "Modérateur Discord"){
+    let args = message.content.split(" ").slice(1);
+    let thingToEcho = args.join("")
+    var embed = new Discord.RichEmbed()
+      .setDescription("Sondage")
+      .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x: ")
+      .setColor("0xB40404")
+      .setTimestamp()
+  message.guild.channels.find("name", "sondage").sendEmbed(embed)
+  .then(function (message){
+    message.react("Oui")
+    message.react("Non")
+  }).catch(function(){
 
+    });
+  }else{
+    return message.reply("Tu n'as pas la permission.")
+}}})*/
 client.on('message', message => {
             
-  if (message.content.startsWith("-sondage")){
+  if (message.content === '-sondage'){
       
     let msg = message.content.split(' ').slice(1);
     let thingToEcho = msg.join("")
