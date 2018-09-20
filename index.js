154fb86f4be01010b590924a245dd8a3e6ae5105
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-20 16:06:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,21 +57,6 @@ client.on('message', message => {
 
   } return })
 
-    client.on('message', message => {
-      
-      if(message.content.startWith(prefix + "clear")){
-               
-        let args = message.content.split(" ").slice(1);
-    
-        if(args >= 100) return message.channel.send("Vous ne pouvez pas clear plus de 100 messages en une fois.")
-            
-        if(!args[0]) return message.channel.send("Vous n'avez pas précisé le nombre de messages à supprimer.")
-        message.channel.bulkDelete(args[0]).then(() => {
-            message.channel.send(`${args[0]} messages ont été supprimés !`);
-        })
-  } return })
-
-
           client.on('message', message => {
             const args = message.content.slice(prefix.length).trim().split(/ +/g);
             const command = args.shift().toLowerCase();
