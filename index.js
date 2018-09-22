1d06560c3f58f296a2245e6ddc10f68793796954
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-22 18:15:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ if (message.content.startsWith(prefix + 'euromillion')) {
 
 client.on("message", (message) => {
 	if (message.content.startsWith(prefix + 'loto')) {
-		var maissuite = ['1', '2', "3", '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90'];
+		var maissuite = ['1', '2', "3", '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50'];
 		var maissuiterdm = Math.floor(Math.random()*maissuite.length);
 		message.delete()
 		message.channel.sendMessage("Voici le numéro: **"  + maissuite[maissuiterdm] +"**")
@@ -216,7 +216,7 @@ client.on('message', message => {
       .addField("Nom du Discord", message.guild.name)
       .addField("Crée le", message.guild.createdAt)
       .addField("Tu as rejoint le", message.member.joinedAt)
-      .addField("Utilisateur sur le Discord", message.guild.memberCount)
+			.addField("Utilisateur sur le Discord", message.guild.memberCount)
       .addField("Nom du BOT", client.user.username)
       .setColor('#01FE23')
       .setImage("https://i.imgur.com/A1wcXrl.png")
