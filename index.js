1d8b0ee7ba68b657a1f58a552837b86b619a5657
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-22 17:05:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ if (message.content.startsWith(prefix + 'euromillion')) {
 	var maissuiterdm5 = Math.floor(Math.random()*maissuite5.length);
 	var maissuite6 =['1', '2', "3", '4', '5', '6', '7', '8', '9'];
 	var maissuiterdm6 = Math.floor(Math.random()*maissuite6.length);
+	message.delete()
 	message.channel.sendMessage("Voici les numéro de l'euromillion")
 	message.channel.sendMessage("Les numéros: **"  + maissuite[maissuiterdm] +"** **" +  maissuite2[maissuiterdm2] + "** **" + maissuite3[maissuiterdm3] +"** **" + maissuite4[maissuiterdm4] + "** **" + maissuite5[maissuiterdm5] + "** n° complémentaire **" + maissuite6[maissuiterdm6] + "**")
 }
@@ -52,6 +53,7 @@ client.on("message", (message) => {
 	if (message.content.startsWith(prefix + 'loto')) {
 		var maissuite = ['1', '2', "3", '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90'];
 		var maissuiterdm = Math.floor(Math.random()*maissuite.length);
+		message.delete()
 		message.channel.sendMessage("Voici le numéro: **"  + maissuite[maissuiterdm] +"**")
 	}
 	})
