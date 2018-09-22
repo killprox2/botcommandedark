cfadbf740d14e69c3d01519d7d8fcc99edc8cda9
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-09-22 23:32:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -300,13 +300,20 @@ client.on('message', message => {
                 let object = args[0];
                 let detail = args.slice(1).join(" ");
                 let hereRole = message.guild.roles.find("name", "Forgemages");
-                if(!object) object = "Aucun object indiqué";
-                if(!detail) detail = "Aucune information indiqué";
-                message.channel.send(":white_check_mark: Votre commande a été envoyée, un forgemage va prendre contact avec vous dès qu'il sera disponible")
+								if(typeof commande[1] === 'undefined')
+								{
+									
+										// Nom d'utilisateur pas entré = afficher l'aide
+										message.reply("**Aide pour la commande report :** \n\n Pour rapporter un ou plusieurs utilisateurs ayant un comportement inapproprié, mettre le nom ou les noms des utilisateurs après la commande report. \n\n Vous pouvez également rajouter une raison particulière avec l'attribut `!r:\"Votre raison\"`. \n\n Ne vous amusez pas à abuser cette commande à tout va, merci :wink: ! \n\n **Exemple 1 :** `-report @user` \n **Exemple 2 :** `-report @user1 @user2` \n **Exemple 3 :** `-report @user1 !r:\"Une raison\"`");
+									
+								}
+								else
+								{
+                    message.channel.send(":white_check_mark: Votre commande a été envoyée, un forgemage va prendre contact avec vous dès qu'il sera disponible")
                
                     message.delete()
                     message.guild.channels.find("name", "liste-commande-fm").send(hereRole + ` Salut le joueur **${message.author.username}** souhaiterait une FM. Prenez contact avec lui dès que vous êtes disponible pour cette commande directement dans le jeu ou en MP Discord ! Voici sa commande: ${object}. Informations supplémentaires sur la FM: J'ai besoin de : ${detail}`);
                      // message.channel.send(hereRole + ` Salut `+ 'le joueur ' + message.member.displayName + ` souhaiterai: ${object}. Information supplémentaire sur le FM: ${detail}`);
-                   
+								}
             } return }
           )
