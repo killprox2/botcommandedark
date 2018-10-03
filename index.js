a94d22e3d15985bcc5670380ba31b7d6f2bccef1
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-10-03 03:17:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -374,7 +374,7 @@ client.on('message', message => {
             let object = args[0];
                 let detail = args.slice(1).join(" ");
 								let hereRole = message.guild.roles.find("name", "Forgemages");
-           
+								message.delete()
 								if(command === "fm"){
 									if(!object){
 											var err_code = new Discord.RichEmbed()
