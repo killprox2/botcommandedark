d52700274669639a1a12f1e1259356bacd964479
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-10-03 03:15:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -379,16 +379,14 @@ client.on('message', message => {
 									if(!object){
 											var err_code = new Discord.RichEmbed()
 											.setTitle('Error 400 - Bad Request')
-											.setDescription("Tu n\'a pas précisé l'object!")
-											.addField("**-fm**", "Object Imformation")
+											.setDescription("Tu n\'a pas précisé l'object! :warning: -fm Object + Imformation")
 											.setColor('#e74c3c')
 											message.channel.send(err_code);
 									
 									}else if(!detail){
 											var err_code = new Discord.RichEmbed()
 											.setTitle('Error 400 - Bad Request')
-											.setDescription("Tu n\'a pas précisé les imformations")
-											.addField("**-fm**", "Object Imformation")
+											.setDescription("Tu n\'a pas précisé les imformations :warning: -fm Object + Imformation")
 											.setColor('#e74c3c')
 											message.channel.send(err_code);
 									
