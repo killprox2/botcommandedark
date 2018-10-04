066b24657e88200be2d36338480a73fc23e913e1
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-10-04 02:20:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const prefix = "-";
-const ms = require("ms");
 let os = require('os')
 
 client.on('ready', function () {
