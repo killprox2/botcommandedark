aacc627013f0cea83a26690046e50f3ec33540eb
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-10-05 01:00:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -332,9 +332,9 @@ client.on('message', message => {
 
   } return })
 		client.on('message', (message) => {
+			var command = args6.shift().toLowerCase();
 				let args6 = message.content.split(' ').slice(1);
 				let question = args6.slice(0).join(" ");
-				var command = args.shift().toLowerCase();
 			if (command === "-sondage1") {
 				if(!message.member.hasPermission("MANAGE_MESSAGES")) {
 					var pollEmbed = new Discord.RichEmbed()
