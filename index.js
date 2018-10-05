a14cd9ee6cb0492d1e8060c2da591d2527584548
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-10-05 21:52:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,27 +51,7 @@ if (message.content.startsWith(prefix + 'euromillion')) {
 	message.channel.sendMessage("Voici les numéro de l'euromillion")
 	message.channel.sendMessage("Les numéros: **"  + maissuite[maissuiterdm] +"** **" +  maissuite2[maissuiterdm2] + "** **" + maissuite3[maissuiterdm3] +"** **" + maissuite4[maissuiterdm4] + "** **" + maissuite5[maissuiterdm5] + "** n° complémentaire **" + maissuite6[maissuiterdm6] + "**")
 }
-})
-
-client.on("message", (message) => {
-	if (message.content.startsWith(prefix + 'tloto')) {
-		if(!message.member.hasPermission("MANAGE_MESSAGES")) {
-			var pollEmbed = new Discord.RichEmbed()
-			.setDescription('Pas la permission "MANAGE_MESSAGES".')
-			.setColor(color)
-			message.channel.send(pollEmbed)
-		}}else if(message.member.hasPermission("MANAGE_MESSAGES")) {
-		var maissuite = ['01', '02', "03", '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '50'];
-		//var maissuite = ['01', '02', "03", '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90'];
-		var maissuiterdm = Math.floor(Math.random()*maissuite.length);
-      var embed = new Discord.RichEmbed()
-      .setTitle("#__**LOTO**__#")
-      .setColor(0x00AE86)
-			.addField("Félicitation à ", maissuite[maissuiterdm])
-			message.channel.sendEmbed(embed)
-	}return
-	})
-	
+})	
 	client.on('message', message => {
 		if (message.content === '-loto') {   
 		  message.delete()
@@ -132,6 +112,24 @@ client.on("message", (message) => {
 		message.channel.sendEmbed(embed)
 		}return
 	  })
+	  client.on("message", (message) => {
+		if (message.content.startsWith(prefix + 'tloto')) {
+			if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+				var pollEmbed = new Discord.RichEmbed()
+				.setDescription('Pas la permission "MANAGE_MESSAGES".')
+				.setColor(color)
+				message.channel.send(pollEmbed)
+			}}else if(message.member.hasPermission("MANAGE_MESSAGES")) {
+			var maissuite = ['01', '02', "03", '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '50'];
+			//var maissuite = ['01', '02', "03", '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90'];
+			var maissuiterdm = Math.floor(Math.random()*maissuite.length);
+		  var embed = new Discord.RichEmbed()
+		  .setTitle("#__**LOTO**__#")
+		  .setColor(0x00AE86)
+				.addField("Félicitation à ", maissuite[maissuiterdm])
+				message.channel.sendEmbed(embed)
+		}return
+		})
 
 client.on("message", (message) => {
 	
