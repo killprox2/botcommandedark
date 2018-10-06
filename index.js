c44e3e35967fbd12e67bc3e6007ca7a3c528b288
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-10-06 21:17:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const Fornite = require('fortnite')
+const stats = new Fornite('773fc2ed-54d9-4c97-befa-99f2cc3da8a8')
 const prefix = "-";
 let os = require('os')
 
@@ -162,7 +164,50 @@ if (message.content.startsWith(prefix + 'euromillion')) {
 			.setFooter("#__**DarkBot**__# by darkvince37")
 		message.channel.sendEmbed(embed)
 		}return
-	  })
+		})
+
+		client.on("message", (message) => {
+			if(command === "fortnite") {
+
+			const args = message.content.slice(prefix.length).trim().split(/ +/g);
+			const command = args.shift().toLowerCase();
+	
+			let platform;
+			let username;
+	
+			if (!['pc','xbl','psn'].includes(args[0])) return message.channel.send('Dit moi la platform "-fortnite [pc I xbl I psn ] <username>" ')
+			if (!args[1]) return message.channel.send('Dit moi l\'utilisateur! "-fortnite plateforme user" ')
+	
+			platform = args[0];
+			username = args[1]
+	
+	
+	
+			stats.getInfo(username, platform).then( data => {
+			
+			var embed = new Discord.RichEmbed()
+		.setTitle(`Stats pour ${data.username}`)
+		.setColor('#FF0000')
+		.setThumbnail('https://static-pepper.dealabs.com/threads/thread_large/default/1178780_1.jpg')
+		.addField('Score total', data.lifetimeStats[6].value, true)
+		.addField('Games jouées', data.lifetimeStats[7].value, true)
+		.addField('Victoires', data.lifetimeStats[8].value, true)
+		.addField('Pourcentage de win', data.lifetimeStats[9].value, true)
+		.addField('Kills', data.lifetimeStats[10].value, true)
+		.addField('K/D ratio', data.lifetimeStats[11].value, true)
+		.addField('Temps de jeu', data.lifetimeStats[13].value, true)
+		.addField('Temps moyen de survie', data.lifetimeStats[14].value, true);
+	
+	message.channel.send({embed});
+	
+			})
+			.catch(error => {
+				 message.channel.send('Deso, pas trouver ce joueur')
+			});
+	message.delete()
+		}
+			})
+		
 	  client.on('message', message => {
 		if (message.content === '-iloto') {   
 		  message.delete()
