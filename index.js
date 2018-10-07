24df406b993c89c21d3c7bc9d232e84997b68dd7
killprox2 <43413557+killprox2@users.noreply.github.com>
2018-10-07 09:29:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ if (message.content.startsWith(prefix + 'euromillion')) {
 			.setColor("0xB40404") 
 			.addField("**1.**", "Pour participer MP Scharky in Game")
 			.addField("**2.**", "FIN des inscriptions 13/10/18")
-			.addField("**3.**", "Il est possible que le même pseudo soit tiré au sort à plusieurs reprises. Le joueur peu gagné que le premier lot sur le quelle il est tombé ")
+			.addField("**3.**", "Il est possible que le même pseudo soit tiré au sort à plusieurs reprises. Le joueur peut ne peut gagner que le premier lot sur lequel il est tombé ")
 
 
 			message.channel.sendEmbed(embed)
