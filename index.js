4e1c8452e557bd1a4a1b73cc14033f6a57c01cf6
killprox2 <43413557+killprox2@users.noreply.github.com>
2019-03-16 23:10:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -88,33 +88,6 @@ function handleDisconnect() {
 
 handleDisconnect();
 
-function handleDisconnect2() {
-  connection = mysql.createConnection({
-		host: "sql3.cluster1.easy-hebergement.net",
-		user: "darkpandore3",
-		password: process.env.MDP,
-		database: "darkpandore3"
-	}); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect2, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect2();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect2();
 
 client.on('message', message  => {
 	if(message.author.bot || message.channel.type == "dm") return;
@@ -126,7 +99,7 @@ client.on('message', message  => {
 													if(!object){
 																	var err_code = new Discord.RichEmbed()
 																	.setTitle('Error 400 - Bad Request')
-																	.setDescription("Voici un exemple -loto 1 darkvince_")
+																	.setDescription("Voici un exemple -lotoadd 1 darkvince_")
 																	.setColor('#e74c3c')
 																	message.channel.send(err_code);
 													 
@@ -147,14 +120,13 @@ client.on('message', message  => {
 									
 																					.setColor('#8e44ad')
 																					
-																						var sql = "INSERT INTO loto (numero, pseudo) VALUES ('"+ object +"', '" + detail + "')";
+																						var sql = "INSERT INTO loto (numero, pseudo) VALUES ('object', 'detail')";
 																						var sql = "SELECT numero, pseudo FROM loto)";
 																						connection.query(sql, function (result) {
 																							
 																							console.log(result);
 																							message.channel.send(code);
 																							message.channel.send("Voici la nouvelle liste")
-																					message.guild.channels.find("name", "test_admin").send(" Salut le joueur @"+ message.author.username +" est absent jusqu'au **"+ object +"** . Informations supplémentaires : **" + detail + "**");
 																						})
 																					}} 
 																						});
