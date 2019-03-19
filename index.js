965f770174cf40bdca1787235db1459e421f1a51
killprox2 <43413557+killprox2@users.noreply.github.com>
2019-03-19 15:19:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -124,13 +124,13 @@ client.on('message', message => {
 				.setDescription('Tu n\'a pas précisé le nombre de joueur 99 max!')
 				.setColor('#e74c3c')
 				message.channel.send(err_code);
-}else if(!message.member.hasPermission("MANAGE_MESSAGES")){
+}else if(message.member.hasPermission("MANAGE_MESSAGES")){
 				var err_code = new Discord.RichEmbed()
 				.setTitle('Error 403 - Unauthorized')
 				.setDescription('Tu n\'a pas la permission d\'executer cette commande !')
 				.setColor('#e74c3c')
 				message.channel.send(err_code);
-}else (message.member.hasPermission("MANAGE_MESSAGES")){
+}else{
 
 var code = new Discord.RichEmbed()
 																	
