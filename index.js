2910f9f7c203be3e9d3e2eb604244d11b3d44c20
killprox2 <43413557+killprox2@users.noreply.github.com>
2019-03-20 15:07:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,37 @@ client.on('message', message => {
 		message.channel.send(`http://www.darkpandore.com/listabsence.php`);
   }; return
 })
+client.on('message', message => {
+		const args = message.content.slice(prefix.length).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
+		if(command === "dm") {
+			let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+			if(!message.member.hasPermission("ADMINISTRATOR"))
+				return message.reply({embed: {
+				  color: 0xC64540,
+				  description: "Pas la permission."
+				}});
+			let DM = args.join(" ").slice(0);
+		  if (!DM) return message.channel.send({embed: {
+			color: 0xC64540,
+			description: `${message.member} S'il vous plaît entrez un message à dm pour les joueurs du discord.`
+		  }});
+		  message.guild.members.forEach((player) => {
+			  message.guild.members.get("351809725513465867").send({embed: {
+				color: 0x00c1c1,
+				title: `**~~-+-------------[-~~ __Dark_BOT__ ~~-]------------+-~~**`,
+				description: `${DM}
+				`+ ` ***Message de ` +` **${message.author.username}*** `
+				
+				
+			  }});
+		  });
+		  message.channel.send({embed: {
+			color: 0xC64540,
+			description: ":white_check_mark: Tous les joueurs de ce serveur discord ont reçu votre message."
+		}});
+	  }
+})
 /*client.on('message', message => {
 	if(message.author.bot || message.channel.type == "dm") return;
 							const args = message.content.slice(prefix.length).trim().split(/ +/g);
