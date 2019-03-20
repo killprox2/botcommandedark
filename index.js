0d0e18ed937a145cc11dfcbd6db8a0a6767b1b4c
killprox2 <43413557+killprox2@users.noreply.github.com>
2019-03-20 14:39:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ client.on('ready', function () {
 })
 
 client.login(process.env.TOKEN)
+	client.on('message', message => {
+		const args = message.content.slice(prefix.length).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
+		if(command === "dm") {
+			let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+			if(!message.member.hasPermission("ADMINISTRATOR"))
+				return message.reply({embed: {
+				  color: 0xC64540,
+				  description: "Pas la permission."
+				}});
+		    let object = args[0];
+			let DM = args.join(" ").slice(0);
+		  if (!DM) return message.channel.send({embed: {
+			color: 0xC64540,
+			description: `${message.member} S'il vous plaît entrez un message à dm pour le joueur du discord.`
+		  }});
+	  
+		  message.guild.members.forEach((player) => {
+			  message.guild.member(object).send({embed: {
+				color: 0x00c1c1,
+				title: `**~~-+-------------[-~~ __Dark_BOT__ ~~-]------------+-~~**`,
+				description: `${DM}
+				`+ ` ***Message de ` +` **${message.author.username}*** `
+				
+				
+			  }});
+		  });
+		  message.channel.send({embed: {
+			color: 0xC64540,
+			description: ":white_check_mark: Tous les joueurs de ce serveur discord ont reçu votre message."
+		}});
+	  }
+})
 
 client.on('message', message => {
   if (message.content === '-abs') {   
