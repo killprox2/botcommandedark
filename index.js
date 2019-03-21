43231b2480050ce40ad37417951c9594a1eaf6aa
killprox2 <43413557+killprox2@users.noreply.github.com>
2019-03-21 17:12:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ client.on('message', (message) => {
         message.channel.send(code);
         message.guild.channels.find('name', 'test').send(`**${pseudo}** avec son **${classe}** niveau **${lvl}** s'est inscrit à l'event`);
    	    message.guild.channels.find('name', 'test').send(`Il y a maintenant **undefined** d'inscrits pour cet event`);
-        message.guild.channels.find('name', ':tada:event_en_cours').send(`Il y a maintenant **undefined** d'inscrits pour cet event`);
+        message.guild.channels.find('name', 'event_en_cours').send(`Il y a maintenant **undefined** d'inscrits pour cet event`);
      
       } catch (err) {
         console.log(err);
