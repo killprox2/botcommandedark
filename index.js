0a8ecd71a2b182c1dc15a79d023513cbddbd4fe9
killprox2 <43413557+killprox2@users.noreply.github.com>
2019-03-21 16:20:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ client.on('message', (message) => {
           .setDescription(':white_check_mark: Votre inscription a bien était envoyé')
           .setColor('#8e44ad');
         message.channel.send(code);
-        message.guild.channels.find('name', 'test').send(`${pseudo} avec sont ${classe} lvl ${lvl}ces inscrits a l'event`);
+        message.guild.channels.find('name', 'test').send(`**${pseudo}** avec sont **${classe}** lvl **${lvl}** ces inscrits a l'event`);
       } catch (err) {
         console.log(err);
       }
