72d27eb3cde495e21dca872bfd5fd88d5f08d18e
killprox2 <43413557+killprox2@users.noreply.github.com>
2019-03-22 15:39:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -866,7 +866,7 @@ client.on('message', (message) => {
     }
   }
 });
-client.on('message', (message) => {
+/*client.on('message', (message) => {
   if (message.content === '-fm') {
     message.delete();
     const embed = new Discord.RichEmbed()
@@ -877,20 +877,7 @@ client.on('message', (message) => {
       .addField('**Information a suivre:**', "Le jet voulu / Les marges possible, si tu veux du perf perf ou si tu accepte -5 vita ou -2 stats par exemple / Le prix / La date de livraison")
       .addField('**-fmgo informations**', "Pour passé la commande")
     message.channel.sendEmbed(embed);
-  }
-
-/*client.on('message', (message) => {
-  if (message.author.bot || message.channel.type == 'dm') return;
-  if (command === 'fm') {
-    const code = new Discord.RichEmbed()
-      .setTitle('Instruction passage commande FM :')
-      .setDescription(':white_check_mark: Merci de précisé les informations nécessaires:')
-      .setField('Le jet voulu / Les marges possible, si tu veux du perf perf ou si tu accepte -5 vita ou -2 stats par exemple / Le prix / La date de livraison')
-      .setFiel('Pour passé la commande fait -fmgo les informations')
-      .setColor('#8e44ad');
-    message.channel.send(code);
-  }
-});*/
+  }*/
 
 client.on('message', (message) => {
   if (message.author.bot || message.channel.type == 'dm') return;
